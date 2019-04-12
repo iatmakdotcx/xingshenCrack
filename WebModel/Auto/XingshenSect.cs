3794c289e7d523325003eeaaf63a2b2d70c48cc7
Mak <i@mak.cx>
2019-04-13 00:27:27 +0800

    机器人帮派信息模型

---
+++ WebModel/Auto/XingshenSect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Web.Model
+{
+    [AttrTableName("rbt_sect")]
+    [Serializable]
+    public class XingshenSect : XingshenSect_BLL
+    {
+        public XingshenSect() { defaultValue(this); }
+        public XingshenSect(int id) { defaultValue(this); RefreshFromId(id); }
+        [AttrTableFieldInfo(DbType.Int32, IsIdentity = true, IsPrimaryKey = true)]
+        public int id { get; protected set; }
+
+        [AttrTableFieldInfo(DbType.String, 100)]
+        public string uuid { get; set; }
+        [AttrTableFieldInfo(DbType.String, 100)]
+        public string sectName { get; set; }
+        [AttrTableFieldInfo(DbType.String, 100)]
+        public string sectid { get; set; }
+        [AttrTableFieldInfo(DbType.DateTime)]
+        public DateTime lastdonate { get; set; }
+
+    }
+}
---
+++ WebModel/BLL/XingshenSect_BLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Web.Model
+{
+    public class XingshenSect_BLL:ModelBase
+    {
+        protected void defaultValue(XingshenSect model)
+        {
+            model.lastdonate = DateTime.Now;
+        }
+        public static XingshenSect GetModel(string uid)
+        {
+            return GetModelWhere<XingshenSect>("uuid=@uid", dbh.MakeInParam("uid", uid));
+        }
+
+
+    }
+
+}
